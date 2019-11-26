699981d830b7ca0c5b15602540efe79caa8397ff
Mirko <mirkopalancaji@gmail.com>
2019-11-27 09:56:44 +1100

    Added context

---
+++ src/components/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import UserCreate from './UserCreate';
+
+import LanguageContext from '../contexts/LanguageContext';
+
+class App extends React.Component {
+  state = { language: 'english' };
+
+  onLanguageChange = language => {
+    this.setState({ language: language });
+  };
+
+  render() {
+    return (
+      <div className="ui container">
+        <div className="">
+          Select a language:
+          <i
+            className="flag us"
+            onClick={() => this.onLanguageChange('english')}
+          />
+          <i
+            className="flag nl"
+            onClick={() => this.onLanguageChange('dutch')}
+          />
+        </div>
+        <LanguageContext.Provider value={this.state.language}>
+          <UserCreate />
+        </LanguageContext.Provider>
+      </div>
+    );
+  }
+}
+
+export default App;
